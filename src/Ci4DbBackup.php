d46e3edbfdded3572e9de35c7ce55cf627ba08ea
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2024-06-06 15:58:09 +0300

    initial commit

---
+++ src/Ci4DbBackup.php
@@ -0,0 +1,14 @@
+<?php
+
+class Ci4DbBackup
+{
+  public function backup()
+  {
+    return 'backing up';
+  }
+
+  public function restore()
+  {
+    return 'restoring';
+  }
+}
---
+++ tests/Ci4DbBackupTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class Ci4DbBackupTest
+{
+  public function backupTest()
+  {
+    return 'backing up';
+  }
+
+  public function restoreTest()
+  {
+    return 'restoring';
+  }
+}
